ecda1154418cb34de6a06946dd0fc06416ac9d1d
ManashAnand <anandmanash321@gmail.com>
2024-11-04 08:28:12 +0530

    added tags using dynamic

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -33,8 +33,8 @@ const UsefulLinksField = dynamic(() => import('@/components/custom/UseFullLinksF
     loading: () => <div>Loading Tags...</div>,
     ssr: false
   })
-
-
+ 
+// afsd 
 
 const formSchema = z.object({
     firstName: z.string().min(2, { message: "firstName must be at least 2 characters." }),
